f647c506c4407fa2151538990bd956f698ca7bc7
ashaasarkaar <ashaasarkaar@gmail.com>
2026-03-05 15:13:39 +0600

    Array of Array ba 2D

---
+++ keys-values.js
@@ -0,0 +1,7 @@
+const king = {name: 'Musafa', age: 55, kingdom: 'Pride Lands'};
+const keys = Object.keys(king);
+const values = Object.values(king);
+const entries = Object.entries(king);
+console.log(keys)
+console.log(values)
+console.log(entries) //Array of Array ba 2D Array bola hoi. ai khane array ar moddhe r akta array create hobe, r sob gulo key + value mile pair akare thakbe, ai ta lagbe
